f968c0d9a3a158486220e13c92f38bdac69111e4
Macarena Parra <mdp_parra@hotmail.com>
2025-03-06 17:30:00 -0600

    cambios

---
+++ ClientePollito/Models/ConexionCorralModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientePollito.Models
+{
+    public partial class ConexionCorralModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string? ip;
+        [ObservableProperty]
+        private string? nombre;
+        [ObservableProperty]
+        private string? pollito;
+    }
+}
