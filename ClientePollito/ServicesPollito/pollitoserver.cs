071d7dbebb1350aba73879953eb28f908026144a
Duarte Angeles Evelyn Azeneth <211G0891@rcarbonifera.tecnm.mx>
2025-04-21 11:12:18 -0600

    pollitoserver

---
+++ ClientePollito/ServicesPollito/pollitoserver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientePollito.ServicesPollito
+{
+    internal class pollitoserver
+    {
+    }
+}
